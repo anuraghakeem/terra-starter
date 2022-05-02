b4e902b2feb58942cd5082f87cb41a8ec5705c14
Anurag Hakeem <anurag@Anurags-Air.Dlink>
2022-05-02 17:25:07 +0530

    updated frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
+import { Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
+import Menu from './components/Menu';
+import WalletAddress from './components/WalletAddress';
 
+//Bring in the required hooks and possible wallet states
+import { useWallet, WalletStatus } from "@terra-money/wallet-provider";
+
+function App() {
+  // Current wallet status, connect & disconnect functions, available connections
+  const { status, connect, disconnect, availableConnectTypes } = useWallet();
+    
+  // Let's take a look at what the starting states are!
+  console.log("Wallet status is ", status);
+  console.log("Available connection types:", availableConnectTypes);
+
+  const renderConnectButton = () => {
+    if (status === WalletStatus.WALLET_NOT_CONNECTED) {
+      return (
+        <div className="connect-wallet-div">
+          <button
+            type="button"
+            key={`connect-EXTENSION`}
+            onClick={() => connect("EXTENSION")}
+            className="cta-button connect-wallet-button"
+          >
+            Connect wallet
+          </button>
+        </div>
+      );
+    }
+    // Check if wallet is connect
+    else if (status === WalletStatus.WALLET_CONNECTED) {
+      return (
+        <button
+          type="button"
+          onClick={() => disconnect()}
+          className="cta-button connect-wallet-button"
+        >
+          Disconnect
+        </button>
+      );
+    }
+  };
+
+  // Nothing changes here :D
   return (
     <main className="App">
       <header>
-        <div className="header-titles">
-          <h1>⚔ Goblin War ⚔</h1>
-          <p>Only you can save us from Goblin town</p>
-        </div>
-
+      <Link to="/" className="home-link">
+          <div className="header-titles">
+            <h1>⚔ Goblin War ⚔️</h1>
+            <p>Only you can save us from Goblin town</p>
+          </div>
+        </Link>
+        <WalletAddress />
       </header>
 
-      <div>
-        <img
-          src="https://media.giphy.com/media/B19AYwNXoXtcs/giphy.gif"
-          alt="Goblin gif"
-        />
-      </div>
+     {/* If not connected, show the goblin GIF! */}
+     {status === WalletStatus.WALLET_NOT_CONNECTED && (
+        <div>
+          <img
+            src="https://media.giphy.com/media/B19AYwNXoXtcs/giphy.gif"
+            alt="Goblin gif"
+          />
+        </div>
+      )}
+
+      {/* Show the menu after connection */}
+      {status === WalletStatus.WALLET_CONNECTED && (
+          <div className="game-menu-container">
+            <Menu />
+          </div>
+        )}
+      {renderConnectButton()}
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,15 +5,33 @@ import ReactDOM from 'react-dom';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+// We import the pages we just created
+import Play from './pages/play';
+import Guide from './pages/guide';
+import Leaderboard from './pages/leaderboard';
 
+// We import Wallet Provider and a util function
+import { getChainOptions, WalletProvider } from '@terra-money/wallet-provider';
+
+const TWITTER_HANDLE = 'anurag_hakeem';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_DISPLAY_NAME= `anu0x.sol`
+
+// Fetch available connection options
+getChainOptions().then((chainOptions) => {
 ReactDOM.render(
   <React.StrictMode>
+    {/* Wrap the app in a context provider for the wallet */}
+    <WalletProvider {...chainOptions}>
       <div className="App-header">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<App />} />
+            {/* Here are the routes we need to declare.*/}
+            {/* These are empty so they will error for now, don't worry! */}
+            <Route path="/play" element={<Play />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/guide" element={<Guide />} />
           </Routes>
         </BrowserRouter>
 
@@ -28,10 +46,11 @@ ReactDOM.render(
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`Made with @${TWITTER_HANDLE}`}</a>
+          >{`@${TWITTER_DISPLAY_NAME}`}</a>
         </div>
       </div>
-
+    </WalletProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+});
